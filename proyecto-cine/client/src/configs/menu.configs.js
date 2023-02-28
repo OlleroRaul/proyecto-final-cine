54a4c562acf2962bb352b06f9ab04e16b4df5ba8
OlleroRaul <95906535+OlleroRaul@users.noreply.github.com>
2023-02-28 14:29:14 +0100

    Add files via upload

---
+++ proyecto-cine/client/src/configs/menu.configs.js
@@ -0,0 +1,59 @@
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import SlideshowOutlinedIcon from "@mui/icons-material/SlideshowOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import RateReviewOutlinedIcon from "@mui/icons-material/RateReviewOutlined";
+import LockResetOutlinedIcon from "@mui/icons-material/LockResetOutlined";
+
+const main = [
+  {
+    display: "inicio",
+    path: "/",
+    icon: <HomeOutlinedIcon />,
+    state: "home"
+  },
+  {
+    display: "películas",
+    path: "/movie",
+    icon: <SlideshowOutlinedIcon />,
+    state: "movie"
+  },
+  {
+    display: "series",
+    path: "/tv",
+    icon: <LiveTvOutlinedIcon />,
+    state: "tv"
+  },
+  {
+    display: "buscar",
+    path: "/search",
+    icon: <SearchOutlinedIcon />,
+    state: "search"
+  }
+];
+
+const user = [
+  {
+    display: "favoritos",
+    path: "/favorites",
+    icon: <FavoriteBorderOutlinedIcon />,
+    state: "favorite"
+  },
+  {
+    display: "reseñas",
+    path: "/reviews",
+    icon: <RateReviewOutlinedIcon />,
+    state: "reviews"
+  },
+  {
+    display: "actualizar contraseña",
+    path: "/password-update",
+    icon: <LockResetOutlinedIcon />,
+    state: "password.update"
+  }
+];
+
+const menuConfigs = { main, user };
+
+export default menuConfigs;
